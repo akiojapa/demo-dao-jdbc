06fb0e6c4d7aa06c483326a34eba23c13414ded6
akiojapa <akioandrei@hotmail.com>
2022-02-18 17:05:54 -0300

    Instantiate pratice
    Melhor forma de instanciar as classes Seller no Databank SQL.

--- src/applications/Program.java
+++ src/applications/Program.java
@@ -14,6 +14,8 @@ public class Program {
 	public static void main(String[] args) {
 		
 		SellerDao sellerdao = DaoFactory.createSellerDao();
+		
+		System.out.println("===== TEST 1: Seller findByID ======");
 		Seller seller = sellerdao.findById(3);
 		
 		System.out.println(seller);
