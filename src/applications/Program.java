1619a5846f6c65ca93eb92ac3182f9e0bebe5071
akiojapa <akioandrei@hotmail.com>
2022-02-18 11:35:44 -0300

    FindByID Implementation
    Implementando a função de encontrar o ID no banco de dados SQL e retornar as informações no console

--- src/applications/Program.java
+++ src/applications/Program.java
@@ -13,12 +13,9 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
+		SellerDao sellerdao = DaoFactory.createSellerDao();
+		Seller seller = sellerdao.findById(3);
 		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-		
-		SellerDao sellerDao = DaoFactory.createSellerDao();
-
 		System.out.println(seller);
 		
 	
